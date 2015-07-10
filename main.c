4b2958b8ff345f47cdcdbc11266f19eae1d71f2e
Inhwa.Choi <tongji70@gmail.com>
2015-07-10 16:35:01 +0900

    Revert "confict test 1"
    This reverts commit e3345b4befa36e21ebddc197acff828c11a26765.

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("Input equation: ");
+	printf("Please enter a expression: ");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
